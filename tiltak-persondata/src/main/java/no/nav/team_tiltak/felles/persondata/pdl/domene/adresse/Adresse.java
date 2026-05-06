e604d0a1b29020c9eae2e37e8f620f985d1b7cde
d142796 <millad.dagdoni@nav.no>
2026-05-06 12:30:54 +0200

    fjernet kommentarer fra Adresse.java

--- tiltak-persondata/src/main/java/no/nav/team_tiltak/felles/persondata/pdl/domene/adresse/Adresse.java
+++ tiltak-persondata/src/main/java/no/nav/team_tiltak/felles/persondata/pdl/domene/adresse/Adresse.java
@@ -1,22 +1,13 @@
 package no.nav.team_tiltak.felles.persondata.pdl.domene.adresse;
 
 public record Adresse(
-	//eksempel = "Bostedsadresse", beskrivelse = "Oppholdsadresse/Kontaktadresse/KontaktinformasjonForDødsbo/"
 	AdresseKildeCode adresseKilde,
-	//eksempel = "NorskPostadresse", beskrivelse = "NorskPostadresse/UtenlandskPostadresse"
 	PostadresseType type,
-	//eksempel = "Postboks 5 St Olavs Plass"
 	String adresselinje1,
-	//eksempel = "adresselinje 2"
 	String adresselinje2,
-	//eksempel = "adresselinje 3
 	String adresselinje3,
-	//eksempel = "0130"
 	String postnummer,
-	//eksempel = "OSLO"
 	String poststed,
-	//eksempel = "NO"
 	String landkode,
-	//eksempel = "Norge"
 	String land)
 {}
