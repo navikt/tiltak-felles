1553ccaea2d7677d1c6f6a4cbd79ab3ffea70a93
d142796 <millad.dagdoni@nav.no>
2026-05-06 08:39:51 +0200

    forenklet kommentarer i Adresse.java

--- tiltak-persondata/src/main/java/no/nav/team_tiltak/felles/persondata/pdl/domene/adresse/Adresse.java
+++ tiltak-persondata/src/main/java/no/nav/team_tiltak/felles/persondata/pdl/domene/adresse/Adresse.java
@@ -1,15 +1,15 @@
 package no.nav.team_tiltak.felles.persondata.pdl.domene.adresse;
 
 public record Adresse(
-	//eksempel = "Bostedsadresse", description = "Oppholdsadresse/Kontaktadresse/KontaktinformasjonForDødsbo/"
+	//eksempel = "Bostedsadresse", beskrivelse = "Oppholdsadresse/Kontaktadresse/KontaktinformasjonForDødsbo/"
 	AdresseKildeCode adresseKilde,
-	//eksempel = "NorskPostadresse", description = "NorskPostadresse/UtenlandskPostadresse"
+	//eksempel = "NorskPostadresse", beskrivelse = "NorskPostadresse/UtenlandskPostadresse"
 	PostadresseType type,
 	//eksempel = "Postboks 5 St Olavs Plass"
 	String adresselinje1,
-	//eksempel = "adresselinje 2", nullable = true
+	//eksempel = "adresselinje 2"
 	String adresselinje2,
-	//eksempel = "adresselinje 3", nullable = true
+	//eksempel = "adresselinje 3
 	String adresselinje3,
 	//eksempel = "0130"
 	String postnummer,
